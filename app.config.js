03f52588f2534585cf19b45750e9ef7859d00bf0
Jayasri1171 <jayasridurgaperumalla@gmail.com>
2025-10-01 20:06:39 +0530

    committttttttttt0000000000000000

--- app.config.js
+++ app.config.js
@@ -2,18 +2,38 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: 'App',
-    slug: 'App',
+    name: 'Ever Green Dairy',
+    slug: 'Ever-Green-Dairy',
     version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/Evergreenlogo.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/Evergreenlogo.png',
+      resizeMode: 'cover',
+      backgroundColor: '#000000'
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/Evergreenlogo.png',
+        backgroundColor: '#ffffff'
+      },
+      edgeToEdgeEnabled: true,
+       package: "com.evergreendiary.app",
+      versionCode: 1
+    },
+    web: {
+      favicon: './assets/favicon.png'
+    },
     extra: {
       BASE_URL: process.env.BASE_URL,
-    //   API_KEY: process.env.API_KEY,
-    },
-    splash: {
-      "image": "./assets/splash-icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
-  },
-  
+      eas: {
+        projectId: "0b5cf1a7-1fd7-4f0e-9a17-8f5dfc9c6f11"
+      }
+    }
+  }
 };
